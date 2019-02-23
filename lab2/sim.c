e160da6b334d127f77e481c698374bd79a4564f3
jswz72 <jacobdasword@gmail.com>
2019-02-23 14:48:59 -0500

    init decode and exec

--- lab2/sim.c
+++ lab2/sim.c
@@ -1,25 +1,99 @@
 #include <stdio.h>
 #include "shell.h"
 
-static uint32_t CUR_OPCODE;
+enum InstructType {R, I, J};
+
+// General
+static uint32_t instruction;
+enum InstructType instruct_type;
+
+// R type or i type
+static uint32_t rs;
+static uint32_t rt;
+
+// R type
+static uint32_t funct;
+static uint32_t rd;
+
+// I type
+static uint32_t imm;
+
+// J type
+static uint32_t addr;
 
 void fetch()
 {
-    uint32_t pc_content = mem_read_32(CURRENT_STATE.PC);
-    CUR_OPCODE = pc_content >> 26;
-    printf("cur opcode: %d", CUR_OPCODE);
+    instruction = mem_read_32(CURRENT_STATE.PC);
+	if (instruction == 0) {
+		RUN_BIT = 0;
+		return;
+	}
 }
+
+void decode_rtype()
+{
+	funct = instruction & 0x3F; // 6 ones
+	instruction >>= 11;
+	rd = instruction & 0x1F; //5 ones
+	instruction >>= 5;
+	rt = instruction & 0x1F; //5 ones
+	instruction >>= 5;
+	rs = instruction & 0x1F; //5 ones
+}
+
+void decode_jtype()
+{
+	addr = instruction & 0X3FFFFFF;
+}
+
+void decode_itype()
+{
+	imm = instruction & 0xFFFF;
+	imm >>= 16;
+	rt = instruction & 0x1F;
+	instruction >>= 5;
+	rs = instruction & 0x1F;
+	instruction >>= 5;
+}
+
 
 void decode()
 {
-    
+    uint32_t opcode = instruction >> 26;
+	if (opcode == 0) {
+		instruct_type = R;
+		decode_rtype();
+	}
+	else if (opcode == 2 || opcode == 3) {
+		instruct_type = J;
+		decode_jtype();
+	}
+	else if (opcode > 3) {
+		instruct_type = I;
+		decode_itype();
+	}
+}
+
+void execute_r()
+{
+}
+
+void execute_i()
+{
 }
 
 void execute()
 {
-    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
-
+	if (instruct_type == R)
+		execute_r();
+	if (instruct_type == J)
+		NEXT_STATE.PC = addr;
+	if (instruct_type == I)
+		execute_i();
+    if (instruct_type != J)
+		NEXT_STATE.PC = CURRENT_STATE.PC + 4;
 }
+
 void process_instruction()
 {
     /* execute one instruction here. You should use CURRENT_STATE and modify
