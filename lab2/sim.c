f25722cd772f3731180c2c6ea93aa51a6e99f280
jswz72 <jacobdasword@gmail.com>
2019-02-23 14:05:29 -0500

    init fetch

--- lab2/sim.c
+++ lab2/sim.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "shell.h"
 
+static uint32_t CUR_OPCODE;
+
 void fetch()
 {
-
+    uint32_t pc_content = mem_read_32(CURRENT_STATE.PC);
+    CUR_OPCODE = pc_content >> 26;
+    printf("cur opcode: %d", CUR_OPCODE);
 }
 
 void decode()
@@ -13,6 +17,7 @@ void decode()
 
 void execute()
 {
+    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
 
 }
 void process_instruction()
