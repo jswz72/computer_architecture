70e15dabc4356be6560875d96000eac004d97f22
jswz72 <jacobdasword@gmail.com>
2019-02-26 16:43:47 -0500

    first pass at all exec funcs

--- lab2/sim.c
+++ lab2/sim.c
@@ -52,10 +52,19 @@ void decode_j()
 	addr = instruction & 0X3FFFFFF;
 }
 
+int32_t sign_extend(int16_t a)
+{
+    int mask = 0x8000;
+    if (mask & a) {
+        a = a | 0xFFFF0000;
+    }
+    return a;
+}
+
 void decode_i()
 {
-	imm = instruction & 0xFFFF;
-	imm >>= 16;
+    imm = sign_extend(instruction & 0xFFFF);
+	instruction >>= 16;
 	rt = instruction & 0x1F;
 	instruction >>= 5;
 	rs = instruction & 0x1F;
@@ -80,33 +89,51 @@ void decode()
 	}
 }
 
-// todo signed vs unsigned?
-// todo flags
+void check_overflow(int32_t a, int32_t b, int32_t res) {
+    if (a > 0 && b > 0 && res < 0
+            || a < 0 && b < 0 && res > 0) {
+        NEXT_STATE.FLAG_V = 1;
+    }
+}
+
 void add() 
 {
-	// todo check and set overflow
-	NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] + CURRENT_STATE.REGS[rt];
+    int32_t rs_val = CURRENT_STATE.REGS[rs];
+    int32_t rt_val = CURRENT_STATE.REGS[rt];
+    int32_t res = rs_val + rt_val;
+    check_overflow(rs_val, rt_val, res);
+    NEXT_STATE.REGS[rd] = res;
 }
+
 void addu() 
 {
 	NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] + CURRENT_STATE.REGS[rt];
 }
+
 void sub() 
 {
-	// todo check and set over(under)flow
-	NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt];
+    int32_t rs_val = CURRENT_STATE.REGS[rs];
+    int32_t rt_val = CURRENT_STATE.REGS[rt];
+    int32_t res = rs_val + rt_val;
+    check_overflow(rs_val, rt_val, res);
+    NEXT_STATE.REGS[rd] = res;
 }
+
 void subu() 
 {
 	NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt];
 }
+
 void slt() 
 {
 	NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] < CURRENT_STATE.REGS[rt];
 }
+
 void sltu() 
 {
-	// todo
+    uint32_t rs_val = CURRENT_STATE.REGS[rs];
+    uint32_t rt_val = CURRENT_STATE.REGS[rt];
+	NEXT_STATE.REGS[rd] = rs_val < rt_val;
 }
 
 void execute_r()
@@ -135,49 +162,68 @@ void execute_r()
 }
 
 
+
+
 void beq()
 {
-	// todo sign extend?
 	int offset = 0;
 	if (CURRENT_STATE.REGS[rs] == CURRENT_STATE.REGS[rt])
-		offset = imm;
+		offset = imm * 4; // todo check
 	inc_pc(offset);
 }
+
 void bne()
 {
-	// todo sign extend?
 	int offset = 0;
 	if (CURRENT_STATE.REGS[rs] != CURRENT_STATE.REGS[rt])
-		offset = imm;
+		offset = imm * 4; // todo check
 	inc_pc(offset);
 }
+
 void bgtz()
 {
 	int offset = 0;
 	if (CURRENT_STATE.REGS[rs] > 0)
-		offset = imm;
+		offset = imm * 4; // todo check
 	inc_pc(offset);
 }
+
 void addi()
 {
+    int32_t rs_val = CURRENT_STATE.REGS[rs];
+    int32_t res = rs_val + imm;
+    check_overflow(rs_val, imm, res);
+    NEXT_STATE.REGS[rt] = res;
 }
+
 void addiu()
 {
+	NEXT_STATE.REGS[rt] = CURRENT_STATE.REGS[rs] + imm;
 }
+
 void slti()
 {
+    NEXT_STATE.REGS[rt] = CURRENT_STATE.REGS[rs] < imm;
 }
+
 void ori()
 {
+    // 0-extend imm
+    NEXT_STATE.REGS[rt] = NEXT_STATE.REGS[rs] | (0X0000FFFF & imm);
 }
+
 void lui()
 {
+    NEXT_STATE.REGS[rt] = imm << 16;
 }
+
 void lw()
 {
+    NEXT_STATE.REGS[rt] = mem_read_32(CURRENT_STATE.REGS[rs] + imm);
 }
 void sw()
 {
+    mem_write_32(CURRENT_STATE.REGS[rs] + imm, CURRENT_STATE.REGS[rt]);
 }
 
 void execute_i()
